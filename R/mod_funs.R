9744d1d0df08ae248753b9878bde057a830867c1
Mark Sorel <mark.sorel@dfw.wa.gov>
2025-01-04 21:37:02 -0800

    improve mod_funs documentation

--- R/mod_funs.R
+++ R/mod_funs.R
@@ -9,17 +9,26 @@
 #' @return a list of functions that return (unoptimized) dlm model objects
 #'
 #' @details
+#'
+#' The models available in this package are variations of a sibling regression model with time-varying intercept and slope:
+#' \deqn{\left\{\begin{aligned}\mathrm{log}(y_{a,t}) & =\alpha_t + \mathrm{log}(y_{a-1,t-1}) \beta_t + v_t, & \quad &v_t \sim \mathcal{N}\left(0, V_t\right), \\\alpha_t & = \alpha_{t-1} + w_{\alpha, t}, & \quad & w_{\alpha, t} \sim \mathcal{N}\left(0, W_{\alpha, t}\right), \\\beta_t & = \beta_{t-1} + w_{\beta, t}, & \quad & w_{\beta, t} \sim \mathcal{N}\left(0, W_{\beta, t}\right),\end{aligned}\right.}
+#'
+#' where \eqn{\alpha_t} and \eqn{\beta_t} are an intercept and a slope for returns of the previous age in the previous year, respectively, and both are allowed to vary across years as random walks with process error variances \eqn{w_{\alpha, t}} and \eqn{w_{\beta, t}} respectively. The residual \eqn{v_t} is assumed to be normally distributed around zero with variance \eqn{V_t}. Together with a vague prior distribution for the values of \eqn{\alpha_0} and \eqn{\beta_0} (the slope and intercept prior to the first year) these equations define the "full" sibling regression models. However, this "full" model may be overly complex for optimal prediction, and is difficult to fit in practice. Simplified versions of this model are included, as described below.
+#'
 #' Model options are:
 #'
 #' - constLM --	Sibling regression with constant slope and intercept.
 #' - tvInt --	Sibling regression with  time-varying intercept.
 #' - tvSlope --	Sibling regression with time-varying slope.
-#' - tvIntSlope --	Sibling regression with time-varying slope and intercept. This model is not inlcuded by default because it generally converges to one of the simpler model. In other words, it is exceedingly rare for the data to support this complex of a model and this including it in an ensemble leads to double weighting one of the simpler models.
+#' - tvIntSlope --	Sibling regression with time-varying slope and intercept (i.e., the "full" mode). This model is not included by default because it generally converges to one of the simpler model. In other words, it is exceedingly rare for the data to support this complex of a model and this including it in an ensemble leads to double weighting one of the simpler models.
 #' - tvCRzeroInt --	Time varying "cohort ratio" model. Time varying slope, Intercept=0.
 #' - constCRzeroInt --	Constant "cohort ratio" model. Constant slope, Intercept=0.
 #' - tvIntOnly --	Time-varying Intercept-only model. Random walk on return, no sibling predictor.
 #' - constIntOnly --	Constant Intercept-only model. Long-term average, no sibling predictor.
 #' - PenDlm -- A dynamic linear model the like "tvIntSlope" but with penalized-complexity priors on the year-to-year variation and the mean of the covariate values. This model also can accommodate additional predictors (e.g., environmental covariates). This model is not included by defaults
+#'
+#'
+#'
 #'
 #' @export
 mod_funs<-function(include=c(
--- R/performance_weights.R
+++ R/performance_weights.R
@@ -1,7 +1,5 @@
 #' Calculates weighted average forecasts, performance metrics, and prediction intervals based on past performance
 #'
-#' @description
-#' Prediction intervals are based on the standard deviation between one-year ahead predictions (i.e., forecasts) and observations in log space. The number of years included in the standard deviation calculations is the minimum of the number of years available or the values specified for `perf_yrs`. For example, if `perf_yrs = 15` but only three years of an ensemble forecast had been generated prior to a given year, those three years would be used to calculate the forecast standard deviation and prediction intervals, however, if 20 years of the ensemble forecast were available, only the most recent 15 years would be used. The "n_sd" field in the output of a call to `forecast_fun()` provides the number of years used to calculate prediction intervals for that forecast. The number of years used to calculate weights for an ensemble forecast is conducted in the same way (i.e., all available up to "wt_yrs") and the "n_wts" field in the output gives the number of years used. This is not relevant for ensembles based on AICc weighting as they are based on the AICc value for models fit for the current year.
 #'
 #'
 #' @param fits data frame returned by `fit_mods` function
@@ -14,10 +12,13 @@
 #' @return a data frame with predictions from component models and weighted average models based on AICc, MAPE, and RMSE weights.
 #'
 #' @details
-#' AICc weights are calculated as:
-#'  $exp(-.5 deltaAICc) / sum(exp(-.5 deltaAICc))$
+#' AICc weights \eqn{w_m} for model \eqn{m} are calculated as:
+#'  \deqn{w_m=\frac{e^{-0.5\left(\mathrm{AIC}_m-\mathrm{AIC}_{\min }\right)}}{\sum_{i \in \mathcal{M}} e^{-0.5\left(\mathrm{AIC}_i-\mathrm{AIC}_{\min }\right)}}}
+#'
 #' MAPE and RMSE weights are calculated as, for example:
-#'  MAPE^-1 / sum(MAPE^-1)
+#'  \deqn{w_m=\frac{\mathrm{MAPE}_m^{-1}}{\sum_{i \in \mathcal{M}} \mathrm{MAPE}_i^{-1}}}
+#'
+#'Prediction intervals are based on the standard deviation between one-year ahead predictions (i.e., forecasts) and observations in log space. The number of years included in the standard deviation calculations is the minimum of the number of years available or the values specified for `perf_yrs`. For example, if `perf_yrs = 15` but only three years of an ensemble forecast had been generated prior to a given year, those three years would be used to calculate the forecast standard deviation and prediction intervals, however, if 20 years of the ensemble forecast were available, only the most recent 15 years would be used. The "n_sd" field in the output of a call to `forecast_fun()` provides the number of years used to calculate prediction intervals for that forecast. The number of years used to calculate weights for an ensemble forecast is conducted in the same way (i.e., all available up to "wt_yrs") and the "n_wts" field in the output gives the number of years used. This is not relevant for ensembles based on AICc weighting as they are based on the AICc value for models fit for the current year.
 #'
 #' - *model_name* is the name of the model used for forecast. It includes the compenent of the ensembles and the ensembles. Additionally, "best" is the model with the lowest MAPE over the perforance-evaluation period for a given year. Thgis is included to be be able to see the performance of picking the best-performaning model, whether than be an ensemble or one of the components, over multiple years. The "Model_name2" field indicates which model was the "best" for that year.
 #' - *n_wts* = the number of years of forecasts and observations that were used to generate MAPE and RMSE weights for ensemble forecasts.
